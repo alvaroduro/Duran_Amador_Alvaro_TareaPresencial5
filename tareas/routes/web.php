34dcf3f767929bc215ac8b98ce00efaf3aab7e97
AlvaroDuran <adurama436@g.educaand.es>
2025-05-20 16:48:09 +0200

    ejercicio 5.1
    - Conexion base datos a traves de PDO

---
+++ tareas/routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Livewire\Volt\Volt;
+use Illuminate\Database\QueryException;
+use Illuminate\Support\Facades\DB;
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::middleware(['auth'])->group(function () {
+    Route::redirect('settings', 'settings/profile');
+
+    Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
+    Volt::route('settings/password', 'settings.password')->name('settings.password');
+    Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
+});
+
+
+Route::get('/test-db', function () {
+    try {
+        DB::connection()->getPdo();
+        return "✅ Conexión exitosa a la base de datos: " . DB::connection()->getDatabaseName();
+    } catch (QueryException $e) {
+        return "❌ No se pudo conectar a la base de datos. Error: " . $e->getMessage();
+    }
+});
+
+require __DIR__.'/auth.php';
